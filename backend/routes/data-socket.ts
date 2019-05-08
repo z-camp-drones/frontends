2771130e52a51e1143dd85575f842015453e26a7
Sebastian Zillessen <sebastian@zillessen.info>
2019-05-08 12:46:05 +0200

    If drone disconnects, reinit the stateEmitter.

--- backend/routes/data-socket.ts
+++ backend/routes/data-socket.ts
@@ -1,77 +1,79 @@
-import {Request, Response, Router} from "express";
+import { Request, Response, Router } from "express";
 import logger from "../commons/logging/logger";
-import {Socket} from "socket.io";
-import {appSocket} from "../app";
-import {mockMessage, random} from './helpers';
+import { Socket } from "socket.io";
+import { appSocket } from "../app";
+import { mockMessage, random } from "./helpers";
 
-const sdk = require('../lib/tellojs');
+const sdk = require("../lib/tellojs");
 
 const router = Router();
 let timer: any = null;
 let stateEmitter: any = null;
 let socket: Socket = null;
 
-router.get('/events', (req: Request, res: Response) => {
-    startEventStream();
-    res.sendStatus(200);
+router.get("/events", (req: Request, res: Response) => {
+  startEventStream();
+  res.sendStatus(200);
 });
-router.delete('/events', (req: Request, res: Response) => {
-    stopEventStream();
-    res.sendStatus(200);
+router.delete("/events", (req: Request, res: Response) => {
+  stopEventStream();
+  res.sendStatus(200);
 });
-router.get('/mocked-events', (req: Request, res: Response) => {
-    startMockEventStream();
-    res.sendStatus(200);
+router.get("/mocked-events", (req: Request, res: Response) => {
+  startMockEventStream();
+  res.sendStatus(200);
 });
 
-router.delete('/mocked-events', (req: Request, res: Response) => {
-    stopMockEventStream();
-    res.sendStatus(200);
+router.delete("/mocked-events", (req: Request, res: Response) => {
+  stopMockEventStream();
+  res.sendStatus(200);
 });
 
 function stopEventStream() {
-    stateEmitter.off('message', pushDataOntoSocket);
+  stateEmitter.off("message", pushDataOntoSocket);
 }
 
 function getSocket() {
-    if (socket === null) {
-        socket = appSocket;
-    }
-    return socket;
+  if (socket === null) {
+    socket = appSocket;
+  }
+  return socket;
 }
 
 function pushDataOntoSocket(message: string) {
-    getSocket().emit('drone-data', message);
+  getSocket().emit("drone-data", message);
 }
 
 function startEventStream() {
-    if (stateEmitter === null) {
-        stateEmitter = sdk.receiver.state.bind();  // Binding to port of state
-    }
-    stateEmitter.on('message', pushDataOntoSocket);
+  if (stateEmitter === null) {
+    stateEmitter = sdk.receiver.state.bind(); // Binding to port of state
+    stateEmitter.on("close", () => {
+      stateEmitter = null;
+    });
+  }
+  stateEmitter.on("message", pushDataOntoSocket);
 
-    // TODO: handle socket disconnect
+  // TODO: handle socket disconnect
 }
 
 function startMockEventStream() {
+  let messageId = 0;
+  let time = 0;
 
-    let messageId = 0;
-    let time = 0;
+  let emitMessage = () => {
+    const message = mockMessage(time);
+    logger.info(`Sending mocked data ${messageId}`);
+    pushDataOntoSocket(JSON.stringify(message));
 
-    let emitMessage = () => {
-        const message = mockMessage(time);
-        logger.info(`Sending mocked data ${messageId}`);
-        pushDataOntoSocket(JSON.stringify(message));
+    messageId += 1;
+  };
+  timer = setInterval(emitMessage, 2000);
 
-        messageId += 1;
-    };
-    timer = setInterval(emitMessage, 2000);
-
-    // TODO: handle socket close to stop interval
+  // TODO: handle socket close to stop interval
 }
 
 function stopMockEventStream() {
-    clearInterval(timer);
+  clearInterval(timer);
 }
 
 export default router;
--- backend/routes/data.ts
+++ backend/routes/data.ts
@@ -1,75 +1,76 @@
-import {Request, Response, Router} from 'express';
-import logger from '../commons/logging/logger';
-import {mockMessage, random} from './helpers';
+import { Request, Response, Router } from "express";
+import logger from "../commons/logging/logger";
+import { mockMessage } from "./helpers";
 
 const router = Router();
 
-const sdk = require('../lib/tellojs');
+const sdk = require("../lib/tellojs");
 
 let stateEmitter: any = null;
 let messageId = 0;
 
 function telloJsServerSideEvents(req: Request, res: Response) {
-    if (stateEmitter === null) {
-        stateEmitter = sdk.receiver.state.bind();  // Binding to port of state
-    }
-
-    let emitMessage = (message: any) => {
-        res.write(`id: ${messageId}\n`);
-        res.write(`data: ${JSON.stringify(message)}\n\n`);
-        messageId += 1;
-    };
-
-    stateEmitter.on('message', emitMessage);
-
-
-    req.on('close', () => {
-        console.error("Connection was closed.");
+  if (stateEmitter === null) {
+    stateEmitter = sdk.receiver.state.bind(); // Binding to port of state
+    stateEmitter.on("close", () => {
+      stateEmitter = null;
     });
+  }
+
+  let emitMessage = (message: any) => {
+    res.write(`id: ${messageId}\n`);
+    res.write(`data: ${JSON.stringify(message)}\n\n`);
+    messageId += 1;
+  };
+
+  stateEmitter.on("message", emitMessage);
+
+  req.on("close", () => {
+    console.error("Connection was closed.");
+  });
 }
 
 function mockedEvents(req: Request, res: Response) {
+  let messageId = 0;
+  let time = 0;
 
-    let messageId = 0;
-    let time = 0;
+  let emitMessage = () => {
+    const message = mockMessage(time);
+    res.write(`id: ${messageId}\n`);
+    res.write(`data: ${JSON.stringify(message)}\n\n`);
+    logger.info(`Sending mocked data ${messageId}`);
+    messageId += 1;
+  };
+  const timer = setInterval(emitMessage, 2000);
 
-    let emitMessage = () => {
-        const message = mockMessage(time);
-        res.write(`id: ${messageId}\n`);
-        res.write(`data: ${JSON.stringify(message)}\n\n`);
-        logger.info(`Sending mocked data ${messageId}`)
-        messageId += 1;
-    };
-    const timer = setInterval(emitMessage, 2000);
-
-    req.on('close', () => {
-        console.error("Connection was closed.");
-        clearInterval(timer);
-    });
+  req.on("close", () => {
+    console.error("Connection was closed.");
+    clearInterval(timer);
+  });
 }
 
-router.get('/events', (req: Request, res: Response) => {
-    // Server-Send-events Setup
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-    });
-    res.write('\n');
+router.get("/events", (req: Request, res: Response) => {
+  // Server-Send-events Setup
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive"
+  });
+  res.write("\n");
 
-    telloJsServerSideEvents(req, res);
+  telloJsServerSideEvents(req, res);
 });
 
-router.get('/mocked-events', (req: Request, res: Response) => {
-    // Server-Send-events Setup
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-    });
-    res.write('\n');
+router.get("/mocked-events", (req: Request, res: Response) => {
+  // Server-Send-events Setup
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive"
+  });
+  res.write("\n");
 
-    mockedEvents(req, res);
+  mockedEvents(req, res);
 });
 
 export default router;
