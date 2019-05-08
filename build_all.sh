fdffc089639963a16d895c5446d033e1626237d8
Sebastian Zillessen <sebastian@zillessen.info>
2019-05-08 10:53:44 +0200

    Remove Nvm use from build scripts

--- build_all.sh
+++ build_all.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
 dir=$(pwd)
-. ~/.nvm/nvm.sh
 
 echo "BUILDING BACKEND"
 cd ${dir}/backend/
-nvm use
-npm build
+npm run build
 
 echo "BUILDING COCKPIT"
 cd ${dir}/frontends/cockpit
-nvm use
-npm build
+npm run build
 
 echo "BUILDING CONTROL-CENTER"
 cd ${dir}/frontends/control-center
-nvm use
-npm build
+npm run build
 
 echo "BUILDING TELEMETRY-STREAM"
 cd ${dir}/frontends/telemetry-stream
-nvm use
-npm build
+npm run build
 
 echo "BUILDING VIDEO-STREAM"
 cd ${dir}/frontends/video-stream
-nvm use
-npm build
+npm run build
--- install_all.sh
+++ install_all.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
 dir=$(pwd)
-. ~/.nvm/nvm.sh
 
 echo "INSTALLING BACKEND"
 cd ${dir}/backend/
-nvm use
 npm install
 
 echo "INSTALLING COCKPIT"
 cd ${dir}/frontends/cockpit
-nvm use
 npm install
 
 echo "INSTALLING CONTROL-CENTER"
 cd ${dir}/frontends/control-center
-nvm use
 npm install
 
 echo "INSTALLING TELEMETRY-STREAM"
 cd ${dir}/frontends/telemetry-stream
-nvm use
 npm install
 
 echo "INSTALLING VIDEO-STREAM"
 cd ${dir}/frontends/video-stream
-nvm use
 npm install
