ce1d2b08f64bbc3ddefb7a9410181e392ce51cc7
Sebastian Zillessen <sebastian@zillessen.info>
2019-05-09 09:15:54 +0200

    Adapt ports of backend app

--- backend/app-config.ts
+++ backend/app-config.ts
@@ -1,5 +1,5 @@
-export const getAppPort = (): number => process.env.PORT ? parseInt(process.env.PORT) : 3000;
+export const getAppPort = (): number => process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
-export const getVideoForwardPort = (): number => process.env.VIDEO_FORWARD_PORT ? parseInt(process.env.VIDEO_FORWARD_PORT) : 8081;
+export const getVideoForwardPort = (): number => process.env.VIDEO_FORWARD_PORT ? parseInt(process.env.VIDEO_FORWARD_PORT) : 8084;
 
-export const getVideoSocketPort = (): number => process.env.VIDEO_SOCKET_PORT ? parseInt(process.env.VIDEO_SOCKET_PORT) : 8082;
+export const getVideoSocketPort = (): number => process.env.VIDEO_SOCKET_PORT ? parseInt(process.env.VIDEO_SOCKET_PORT) : 8083;
